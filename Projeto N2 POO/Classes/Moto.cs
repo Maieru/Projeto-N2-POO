957fb419052ac562e432ed0928f8ef806d4c3522
Maieru Joao <soljoaomaieru@hotmail.com>
2021-05-13 16:08:12 -0300

    Adicionada classe Moto

---
+++ Projeto N2 POO/Classes/Moto.cs
@@ -0,0 +1,17 @@
+﻿using Projeto_N2_POO.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto_N2_POO.Classes
+{
+    class Moto : VeiculoBase, IVeiculoPagaPedagio, IVeiculoPossuiPassageiro
+    {
+        public int CapacidadeDePassageiro { get; set; }
+
+        public string Empinar() => "Empinando.";
+        public double PagarPedagio() => 3;
+    }
+}
