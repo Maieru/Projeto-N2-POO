663a475c43d1021b6a9ec748ea36bf5f30cb9a30
Maieru Joao <soljoaomaieru@hotmail.com>
2021-05-16 02:24:25 -0300

    Adicionado tela de gerencia de pedagios.

---
+++ Projeto N2 POO/Forms/frGerenciarPedagio.cs
@@ -0,0 +1,30 @@
+﻿using Projeto_N2_POO.Classes;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Projeto_N2_POO.Forms
+{
+    public partial class frGerenciarPedagio : frBase
+    {
+        public frGerenciarPedagio()
+        {
+            InitializeComponent();
+
+            if (Dados.Pedagios.Count == 0)
+                ExibeTelaDeErro();
+        }
+        public void ExibeTelaDeErro()
+        {
+            PictureBox pictureBox = new PictureBox();
+            pictureBox.Dock = DockStyle.Fill;
+            pi
+        }
+    }
+}
